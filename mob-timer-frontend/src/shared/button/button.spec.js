7d9a7a07b2670f342435267116fb40de9d965cb7
Enno Lohmann <ennolohmann@ip-192-168-178-26.eu-central-1.compute.internal>
2021-07-22 09:48:44 +0200

    fix test

--- mob-timer-frontend/src/shared/button/button.spec.js
+++ mob-timer-frontend/src/shared/button/button.spec.js
@@ -6,7 +6,7 @@ describe('button',  () => {
     let component;
 
     beforeEach(() => {
-        component = render(Button, {text: 'Test', href: 'test1234'});
+        component = render(Button, {text: 'Test'});
     })
 
     it('should render', () => {
@@ -14,14 +14,20 @@ describe('button',  () => {
     })
 
     it('should render an element with the given text', () => {
-        const {getByText} = component;
+        const {getByText} = component; 
         const linkElement = getByText('Test');
         expect(linkElement).toBeTruthy();
     })
 
-    it('should link to the given href on click', async () => {
-        const {getByText} = component;
-        const linkElement = getByText('Test');
-        expect(linkElement).toHaveAttribute('href', "test1234");
+    it('should pass the on click event', async () => {
+        const onClick = jest.fn();
+        component.component.$on('click', onClick);
+
+        const {getByText} = component; 
+        const buttonElement = getByText('Test');
+        await fireEvent.click(buttonElement);
+        expect(onClick).toHaveBeenCalled();
+
     })
+
 })
